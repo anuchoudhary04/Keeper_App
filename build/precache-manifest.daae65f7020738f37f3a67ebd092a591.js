d446f4ea06948727aad787d214974b239e0f1d4b
Anu choudhary <anuchoudhry04@gmail.com>
2021-06-23 19:50:56 +0530

    first commit

---
+++ build/precache-manifest.daae65f7020738f37f3a67ebd092a591.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea435e5d38ebf15803867fd04d61e4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b50f82e3461311d94aa",
+    "url": "/static/js/2.452d38b9.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.452d38b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64b7b84fe17b24164d42",
+    "url": "/static/js/main.0b1a4a6a.chunk.js"
+  },
+  {
+    "revision": "545623eea297b9a7927a",
+    "url": "/static/js/runtime-main.4fa04daa.js"
+  }
+]);
